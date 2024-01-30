2733855e33fe44c9911d520cf26469eb2d9a2930
Divyanshu Pabia <thedivyanshupabia@gmail.com>
2024-01-30 11:48:28 +0530

    Initialized Project

---
+++ Gamergrid/GamergridApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamergridApp.swift
+//  Gamergrid
+//
+//  Created by Divyanshu Pabia on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GamergridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
