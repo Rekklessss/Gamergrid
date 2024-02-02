10010fa758ff426d50b33c820a3545640641a29b
sarthakk24 <sarthak.sachdeva.73@gmail.com>
2024-02-02 14:38:22 +0530

    Add Tournaments List

---
+++ Gamergrid/Constants/ColorContants.swift
@@ -0,0 +1,14 @@
+//
+//  ColorContants.swift
+//  Gamergrid
+//
+//  Created by user2 on 02/02/24.
+//
+
+import SwiftUI
+
+struct ColorContants {
+    static let TournamentTitle: Color = .init("TournamentTitle")
+    static let TournamentMatches: Color = .init("TournamentMatches")
+    static let TopTitleBar: Color = .init("TopTitleBar")
+}
--- Gamergrid/MatchesView.swift
+++ Gamergrid/MatchesView.swift
@@ -44,10 +44,103 @@ struct MatchesView: View {
                         })
                     }
                 }
-                .padding(.top, getRelativeHeight(20))
+                .padding(.vertical, getRelativeHeight(20))
                 .padding(.horizontal, 10)
+                .background(ColorContants.TopTitleBar)
                 
                 Spacer()
+                
+                ScrollView(.vertical) {
+//                  Loop tournaments
+                    VStack(spacing: 0) {
+//                      Tournament Title and info
+                        HStack(){
+                            HStack(spacing: 20){
+                                AsyncImage(url: URL(string: "https://picsum.photos/id/12/600")) { image in
+                                    image.resizable()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(width: getRelativeWidth(30), height: getRelativeHeight(30))
+                                .clipShape(Circle())
+                                
+                                Text("Roobet Cup 2023")
+                                    .foregroundColor(.white)
+                                    .font(.subheadline)
+                            }
+                            
+                            Spacer()
+                            
+                            Button(action: {}, label: {
+                                Image(systemName: "chevron.down")
+                                    .resizable()
+                                    .frame(width: getRelativeWidth(25), height: getRelativeWidth(15))
+                            })
+                        }
+                        .padding(10)
+                        .background(RoundedCorners(topLeft: 15, topRight: 15).fill(ColorContants.TournamentTitle))
+                        
+//                      Tournament Matches Info
+                        VStack(){
+//                          Loop Matches
+                            HStack {
+                                Spacer()
+                                
+//                              Team 1
+                                Text("Furia")
+                                    .font(.subheadline)
+                                
+                                Spacer()
+                                
+//                              Team1 Image
+                                AsyncImage(url: URL(string: "https://picsum.photos/id/12/600")) { image in
+                                    image.resizable()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(width: getRelativeWidth(30), height: getRelativeHeight(30))
+                                .clipShape(Circle())
+                                
+                                Spacer()
+                                
+//                              Timings
+                                VStack {
+                                    Text("07:30")
+                                    Text("PM")
+                                }
+                                .font(.subheadline)
+                                
+                                
+                                Spacer()
+                                
+//                              Team 2 Image
+                                AsyncImage(url: URL(string: "https://picsum.photos/id/12/600")) { image in
+                                    image.resizable()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(width: getRelativeWidth(30), height: getRelativeHeight(30))
+                                .clipShape(Circle())
+                                
+                                Spacer()
+                                
+//                              Team 2
+                                Text("Astralis")
+                                    .font(.subheadline)
+                                
+                                Spacer()
+                            }
+                            
+                        }
+                        .foregroundColor(.white)
+                        .padding(10)
+                        .background(RoundedCorners(bottomLeft: 15, bottomRight: 15).fill(ColorContants.TournamentMatches))
+                    }
+                    
+                    
+                }
+                .padding(.vertical, getRelativeHeight(30))
+                .padding(.horizontal, getRelativeWidth(10))
             }
             
         }
