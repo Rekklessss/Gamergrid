92f9aff4fba8c54de0aa41977a124c7c489101ac
sarthakk24 <sarthak.sachdeva.73@gmail.com>
2024-02-02 13:06:49 +0530

    Add styles to tab bar

--- Gamergrid/MatchesView.swift
+++ Gamergrid/MatchesView.swift
@@ -13,8 +13,43 @@ struct MatchesView: View {
             Color.black
                 .ignoresSafeArea()
                     
-            Text("This is Matches view")
-                .foregroundColor(.white)
+            VStack {
+                HStack {
+                    Image("MainLogo")
+                        .resizable()
+                        .frame(width: getRelativeWidth(145.0),
+                               height: getRelativeHeight(32.0))
+                        .scaledToFit()
+                        .clipped()
+                    
+                    Spacer()
+                    
+                    HStack(alignment: .top, spacing: getRelativeWidth(15)) {
+                        Button(action: {}, label: {
+                            Image(systemName: "clock.badge")
+                                .resizable()
+                                .frame(width: getRelativeWidth(25), height: getRelativeWidth(25))
+                        })
+                        
+                        Button(action: {}, label: {
+                            Image(systemName: "magnifyingglass")
+                                .resizable()
+                                .frame(width: getRelativeWidth(25), height: getRelativeWidth(25))
+                        })
+                        
+                        Button(action: {}, label: {
+                            Image(systemName: "calendar")
+                                .resizable()
+                                .frame(width: getRelativeWidth(25), height: getRelativeWidth(25))
+                        })
+                    }
+                }
+                .padding(.top, getRelativeHeight(20))
+                .padding(.horizontal, 10)
+                
+                Spacer()
+            }
+            
         }
     }
 }
